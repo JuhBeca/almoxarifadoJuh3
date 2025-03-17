a8d9035b292d447717aa66458df78709a5b1267b
JuhBeca <julliarebeca07@gmail.com>
2025-03-17 11:02:54 -0300

    apenas conferindo

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+becb98c366becf5538ca14fd644ef8dd694b4b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c59333c1419bb1fbeeebef5900222e80420ee17")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
